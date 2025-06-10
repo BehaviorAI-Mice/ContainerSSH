78d5e6db15da929edf11cebf11730f746a3a6d7b
Hojat Sargolzehi <hojats7731@gmail.com>
2025-06-10 13:07:03 -0600

    Fix: find container filter

--- internal/docker/docker_impl.go
+++ internal/docker/docker_impl.go
@@ -200,6 +200,7 @@ func (d *dockerV20Client) findContainer(
 
 	d.backendRequestsMetric.Increment()
 	containers, err = d.dockerClient.ContainerList(ctx, types.ContainerListOptions{
+		All:     true,
 		Filters: filters.NewArgs(filters.KeyValuePair{Key: "name", Value: containerName}),
 	})
 
