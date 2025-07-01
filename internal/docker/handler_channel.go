82df5b27b92cb3d74f9090a605f425c97e92f3a2
Hojat Sargolzehi <hojats7731@gmail.com>
2025-07-01 03:26:52 -0600

    Feat: add check for common gpus

--- internal/docker/handler_channel.go
+++ internal/docker/handler_channel.go
@@ -158,7 +158,10 @@ func (c *channelHandler) handleExecModeSession(
 		_, err = c.session.Stdout().Write([]byte("You are trying to use GPUs (" + " - Used GPU: " +
 			strings.Join(commonGPUs, ", ") + ") While they are in use! Please contact administrator, " +
 			"or change your GPU settings at your dashboard! \n"))
-		return err
+		if err != nil {
+			return err
+		}
+		return nil
 	}
 
 	cnt, state, err := c.networkHandler.dockerClient.findContainer(ctx)
